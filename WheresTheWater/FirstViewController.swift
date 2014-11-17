a9db0c440beabb0a2d12ef539a55283d718d174e
Ben Marshall <ben@benmarshall.co.uk>
2014-11-17 19:53:56 +0000

    Removed old code to inject dummy river data.

--- WheresTheWater/FirstViewController.swift
+++ WheresTheWater/FirstViewController.swift
@@ -20,60 +20,6 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        /*
-        // Dummy routine to set up some river data for test!
-        
-        // Create array of rivers
-        let new_rivers = ["Orchy", "Leven", "Garry", "North Esk", "Tummel", "Tay"]
-        
-        
-        // Get App Delegate
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        let managedContext = appDelegate.managedObjectContext!
-        
-        // Set up Core Data Fetch Request
-        let fetchRequest = NSFetchRequest(entityName: "River")
-        
-        // Fetch Core Data
-        var error: NSError?
-        let fetchedResults = managedContext.executeFetchRequest(fetchRequest, error: &error) as [NSManagedObject]?
-        
-        rivers = fetchedResults!
-        
-        // Check length of list so we dont keep adding more......
-        if  rivers.count < 10 {
-        
-            // Create blank entity
-            let entity = NSEntityDescription.entityForName("River", inManagedObjectContext: managedContext)
-            
-            // Loop on new rivers array
-            for new_river in new_rivers {
-            
-                // Create a new river object
-                let river = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
-                river.setValue(new_river, forKey: "river")
-                
-                // And save the new river to the Rivers Core Data
-                var error: NSError?
-                if !managedContext.save(&error) {
-                    // Debug Output
-                    println("Could not save river to Core Data")
-                }
-                rivers.append(river)
-            }
-        }
-        
-        */
-        
-        // Test fetching JSON data from rainchasers!
-        
-        // TODO: Add funtionality to check timestamps before updating!
-        
-        //RainchasersAPI.deleteAllRivers()
-        //RainchasersAPI.downloadFullRiverData()
-        
-        
         // Get App Delegate
         let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
         let managedContext = appDelegate.managedObjectContext!
